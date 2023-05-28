4a117d273b1cbceb93e25bf36c46c8ca153a0767
Vivallo04 <andresvivallo4@gmail.com>
2023-05-27 22:28:37 -0600

    Base BNF grammar

--- src/main.pl
+++ src/main.pl
@@ -1,3 +1,21 @@
-% Hello World
+% Grammar rules
+sentence --> noun_phrase, verb_phrase.
+noun_phrase --> determiner, noun.
+noun_phrase --> determiner, adjective, noun.
+verb_phrase --> verb.
+verb_phrase --> verb, noun_phrase.
+verb_phrase --> verb, preposition_phrase.
+preposition_phrase --> preposition, noun_phrase.
 
-print "Hello World\n";
+% Lexical rules
+determiner --> [the].
+determiner --> [a].
+adjective --> [red].
+adjective --> [big].
+noun --> [cat].
+noun --> [dog].
+noun --> [ball].
+verb --> [runs].
+verb --> [jumps].
+preposition --> [on].
+preposition --> [over].
