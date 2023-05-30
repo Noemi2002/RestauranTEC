57270febd63a2af1f941433c5ce72dcb2ceaa887
Noemí Vargas <mimi2002@estudiantec.cr>
2023-05-30 00:19:33 -0600

    DB update

---
+++ baseBNF.pl
@@ -0,0 +1,10 @@
+% Elementos léxicos
+articulos(el, los, la, las, un, unos, una, unas).
+pronombres(yo, tu, usted, el, ella, nosotros, vosotros, ustedes, ellos, ellas).
+verbos(comer, tomar, ir, estar, querer, ser, gustar, viajar, salir, vivir, poner, venir, saber, ver, decir, hacer, oir, conocer, tener, esperar).
+adverbios(acaso, quiza, probablemente, posiblemente, tampoco, nunca, no, jamas, cerca, aqui, lejos, ahi, entre, bastante, mucho, demasiado, mas, menos, poco, nada, muy, apenas, luego, hoy, ayer, tarde, temprano, antier, pronto, antes, si, cierto, tambien, claro, regular, bien, mal, mejor, peor, asi, siempre).
+conjunciones(y, e, ni, pero, aunque, sino, o, u, si, como, que, mientras, luego, pues, porque).
+preposiciones(a, ante, bajo, con, contra, de, desde, durante, en, entre, hacia, hasta, para, por, segun, durante, mediante, sobre, sin, tras, pro, con, tras).
+interjecciones(si, no, oh, ay, uf, hola, adios).
+sustantivos(gato, perro, casa, mesa, silla, libro, auto, ciudad, pais, persona).
+adjetivos(grande, pequeño, hermoso, feo, feliz, triste, inteligente, tonto, divertido, aburrido).
