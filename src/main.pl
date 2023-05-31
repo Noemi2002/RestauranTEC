3b547495170a47c4b980d639dfd3c97f0b6f1315
Vivallo04 <andresvivallo4@gmail.com>
2023-05-31 00:01:06 -0600

    Add main and binaries

--- src/main.pl
+++ src/main.pl
@@ -1,21 +1,40 @@
-% Grammar rules
-sentence --> noun_phrase, verb_phrase.
-noun_phrase --> determiner, noun.
-noun_phrase --> determiner, adjective, noun.
-verb_phrase --> verb.
-verb_phrase --> verb, noun_phrase.
-verb_phrase --> verb, preposition_phrase.
-preposition_phrase --> preposition, noun_phrase.
+% Restaurant facts
+restaurant('Restaurant A', 'New York', 'Italian').
+restaurant('Restaurant B', 'London', 'Indian').
+restaurant('Restaurant C', 'Paris', 'French').
+restaurant('Restaurant D', 'New York', 'Mexican').
+% Add more restaurants and their attributes as needed
 
-% Lexical rules
-determiner --> [the].
-determiner --> [a].
-adjective --> [red].
-adjective --> [big].
-noun --> [cat].
-noun --> [dog].
-noun --> [ball].
-verb --> [runs].
-verb --> [jumps].
-preposition --> [on].
-preposition --> [over].
+% Rules for querying and recommending restaurants
+restaurant_suggestion(Location, Cuisine) :-
+  restaurant(Name, Location, Cuisine),
+  format('I recommend ~w in ~w which serves ~w cuisine.', [Name, Location, Cuisine]).
+
+% Inference rules
+recommend_restaurant(Location, Cuisine) :-
+  location(Location),
+  cuisine(Cuisine),
+  restaurant_suggestion(Location, Cuisine).
+
+% Domain knowledge
+location('New York').
+location('London').
+location('Paris').
+
+cuisine('Italian').
+cuisine('Indian').
+cuisine('French').
+cuisine('Mexican').
+
+% Entry point for the expert system
+start_expert_system :-
+  write('Hello, what do you want to eat today?'), nl,
+  write('Please enter your location: '),
+  read(Location),
+  write('Please enter your preferred cuisine: '),
+  read(Cuisine),
+  nl,
+  recommend_restaurant(Location, Cuisine).
+
+% Entry point for the expert system
+:- initialization(start_expert_system).
