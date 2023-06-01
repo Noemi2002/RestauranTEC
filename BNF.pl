8b6d9af7ed906a54875eec9bdd9568c0092c3f3d
Noemí Vargas <mimi2002@estudiantec.cr>
2023-06-01 17:57:27 -0600

    BNF almost functional

--- BNF.pl
+++ BNF.pl
@@ -1,27 +1,67 @@
-% Reglas gramaticales para el BNF
-consulta --> verbo, sustantivo, preposicion, sustantivo.
-consulta --> verbo, preposicion, sustantivo.
-consulta --> verbo, sustantivo.
-consulta --> verbo.
 
-verbo --> [comer].
-verbo --> [buscar].
-verbo --> [encontrar].
+% Elementos léxicos
+:-consult(basebnf).
 
-sustantivo --> [hamburguesas].
-sustantivo --> [pizza].
-sustantivo --> [mariscos].
-sustantivo --> [comida_rapida].
-% Agrega más sustantivos según los tipos de comida y otras características
+% Reglas gramaticales
+oracion --> saludo.
+oracion --> despedida.
+oracion --> saludo, pregunta.
+oracion --> saludo_bot.
+oracion --> pregunta.
+oracion --> respuesta_si.
+oracion --> respuesta_no.
 
-preposicion --> [en].
-preposicion --> [cerca_de].
-preposicion --> [en_la_zona_de].
-% Agrega más preposiciones según tus necesidades
+saludo --> [Saludo], {saludar(Saludo)}.
 
-% Predicado principal para la interpretación de la consulta
-interpretar(Consulta) :-
-    phrase(consulta, Consulta).
+despedida --> [Despedida], {despedir(Despedida)}.
+
+saludo_bot --> saludo, [Bot], {sustantivo(Bot, _, _)}.
+
+pregunta --> sintagma.
+
+respuesta_si --> adverbio, sintagma_descriptivo.
+respuesta_si --> adverbio, sintagma_verbal.
+respuesta_si --> adverbio, sintagma_nominal.
+respuesta_si --> adverbio, sintagma_nominal, adjetivo.
+
+respuesta_no --> adverbio.
+
+sintagma --> sintagma_descriptivo.
+sintagma --> sintagma_preposicional.
+
+sintagma_descriptivo --> adverbio, verbo.
+sintagma_descriptivo --> verbo, adjetivo.
+sintagma_descriptivo --> adverbio, determinante, adjetivo.
+
+sintagma_preposicional --> preposicion, adjetivo, verbo.
+sintagma_preposicional --> preposicion, sintagma_nominal.
+
+sintagma_nominal --> determinante, sustantivo.
+sintagma_nominal --> determinante, sustantivo, verbo.
+sintagma_nominal --> determinante, verbo.
+
+% Predicados para elementos léxicos
+saludar(Saludo) :- saludar(Saludo, _).
+
+despedir(Despedida) :- despedir(Despedida, _).
+
+preposicion([Preposicion|Rest], Rest) :- preposicion(Preposicion).
+
+adverbio([Adverbio|Rest], Rest) :- adverbio(Adverbio).
+
+verbo([Verbo|Rest], Rest) :- verbo(Verbo).
+
+determinante([Determinante|Rest], Rest) :- articulo(Determinante).
+
+sustantivo([Sustantivo|Rest], Rest) :- sustantivos(Sustantivo).
+
+% Regla de prueba
+test :- write('Ingrese una oración: '), read(X), oracion(X, []), write('La oración es válida.'), nl, test.
+
+
+
+
+
 
 
 
