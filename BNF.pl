e520396646e181be2043e2f5b2b7f70367b66eb3
Noemí Vargas <mimi2002@estudiantec.cr>
2023-06-04 12:04:54 -0600

    Recibe oraciones completas

--- BNF.pl
+++ BNF.pl
@@ -1,70 +1,76 @@
-:-consult(basebnf).
-:-consult(basese).
+:-consult('basebnf.pl').
+:-consult('basese.pl').
 
 % Reglas gramaticales
-oracion(Oracion, Resultado):-saludo(Oracion, Resultado).
-oracion(Oracion, Resultado):-saludo(Oracion, Res1), sintagma(Res1, Resultado).
-oracion(Oracion, Resultado):-saludo(Oracion, Res1), sustantivo(Res1, Resultado, _).
-oracion(Oracion, Resultado):-sintagma(Oracion, Resultado).
-oracion(Oracion, Resultado):-adverbio(Oracion, Res1), sintagma_descriptivo(Res1, Resultado).
-oracion(Oracion, Resultado):-adverbio(Oracion, Res1), sintagma_verbal(Res1, Resultado).
-oracion(Oracion, Resultado):-adverbio(Oracion, Res1), sintagma_nominal(Res1, Resultado).
-oracion(Oracion, Resultado):-adverbio(Oracion, Res1), sintagma_nominal(Res1, Resultado).
-% oracion(Oracion, Resultado):-adverbio(Oracion, Res1),
-% sintagma_verbal(Res1, Res2), adjetivo(Res2, Resultado).
-oracion(Oracion, Resultado):-adverbio(Oracion, Resultado).
-oracion(Oracion, Resultado):-sintagma_descriptivo(Oracion, Res1), sintagma_preposicional(Res1, Resultado).
-oracion(Oracion, Resultado):-despedida(Oracion, Resultado).
-oracion(Oracion, Resultado):-despedida(Oracion, Res1), sustantivo(Res1, Resultado, _).
+
+oracion(Oracion, Resultado) :- saludo(Oracion, Resultado).
+oracion(Oracion, Resultado) :- saludo(Oracion, Res1), sintagma(Res1, Resultado).
+oracion(Oracion, Resultado) :- saludo(Oracion, Res1), sustantivo(Res1, Resultado, _).
+oracion(Oracion, Resultado) :- sintagma(Oracion, Resultado).
+oracion(Oracion, Resultado) :- adverbio(Oracion, Res1), sintagma_descriptivo(Res1, Resultado).
+oracion(Oracion, Resultado) :- adverbio(Oracion, Res1), sintagma_verbal(Res1, Resultado).
+oracion(Oracion, Resultado) :- adverbio(Oracion, Res1), sintagma_nominal(Res1, Resultado).
+oracion(Oracion, Resultado) :- adverbio(Oracion, Res1), sintagma_nominal(Res1, Resultado).
+oracion(Oracion, Resultado) :- adverbio(Oracion, Resultado).
+oracion(Oracion, Resultado) :- sintagma_descriptivo(Oracion, Res1), sintagma_preposicional(Res1, Resultado).
+oracion(Oracion, Resultado) :- despedida(Oracion, Resultado).
+oracion(Oracion, Resultado) :- despedida(Oracion, Res1), sustantivo(Res1, Resultado, _).
+
+saludo(Oracion, Resultado) :- saludar1(Oracion, Resultado).
+saludo(Oracion, Resultado) :- saludar2(Oracion, Res1), saludar4(Res1, Resultado).
+saludo(Oracion, Resultado) :- saludar3(Oracion, Res1), saludar5(Res1, Resultado).
+
+despedida(Oracion, Resultado) :- despedir(Oracion, Resultado).
+
+sintagma(Oracion, Resultado) :- sintagma_nominal(Oracion, Res1), sintagma_verbal(Res1, Resultado).
+sintagma(Oracion, Resultado) :- sintagma_nominal(Oracion, Res1), sintagma_descriptivo(Res1, Resultado).
+
+sintagma_preposicional(Oracion, Resultado) :- preposicion(Oracion, Res1), sintagma_nominal(Res1, Resultado).
+
+sintagma_descriptivo(Oracion, Resultado) :- adverbio(Oracion, Res1), verbo(Res1, Resultado).
+sintagma_descriptivo(Oracion, Resultado) :- adverbio(Oracion, Res1), determinante(Res1, Resultado, _).
+
+
+sintagma_nominal(Oracion, Resultado) :- sustantivo(Oracion, Resultado, _).
+sintagma_nominal(Oracion, Resultado) :- sustantivo(Oracion, Res1, _), adjetivo(Res1, Resultado).
+sintagma_nominal(Oracion, Resultado) :- sustantivo(Oracion, Res1, _), adjetivo(Res1, Res2), sintagma_nominal(Res2, Resultado).
+sintagma_nominal(Oracion, Resultado) :- sustantivo(Oracion, Res1, _), adjetivo(Res1, Res2), sintagma_verbal(Res2, Resultado).
+sintagma_nominal(Oracion, Resultado) :- sustantivo(Oracion, Res1, _), adjetivo(Res1, Res2), sintagma_descriptivo(Res2, Resultado).
+sintagma_nominal(Oracion, Resultado) :- sustantivo(Oracion, Res1, _), adjetivo(Res1, Res2), sintagma_preposicional(Res2, Resultado).
+sintagma_nominal(Oracion, Resultado) :- sustantivo(Oracion, Res1, _), adjetivo(Res1, Res2), sintagma_nominal(Res2, Res3), sintagma_preposicional(Res3, Resultado).
+
+% Añade aquí otras reglas que definas para sintagma_nominal
+
+% Ejemplos de reglas adicionales:
+% sintagma_nominal(Oracion, Resultado) :- sustantivo(Oracion, Resultado, _), ...
+% sintagma_nominal(Oracion, Resultado) :- sustantivo(Oracion, Res1, _), adjetivo(Res1, Resultado), ...
+
+% Definición de palabras
+
+% Aquí puedes definir las reglas para los diferentes términos del lenguaje.
+
+% Ejemplo:
+% sustantivo(Oracion, Resultado, Genero) :- member(Oracion, ["hamburguesa"]), Resultado = "hamburguesa", Genero = "femenino".
 
 
 
 
-saludo(Oracion, Resultado):- saludar1(Oracion, Resultado).
-saludo(Oracion, Resultado):- saludar2(Oracion, Res1),  saludar4(Oracion, Resultado).
-saludo(Oracion, Resultado):- saludar3(Oracion, Res1),  saludar5(Oracion, Resultado).
-
-despedida(Oracion, Resultado):- despedir(Oracion, Resultado).
-
-sintagma(Oracion, Resultado):- sintagma_nominal(Oracion, Res1), sintagma_verbal(Res1, Resultado).
-sintagma(Oracion, Resultado):- sintagma_nominal(Oracion, Res1), sintagma_descriptivo(Res1, Resultado).
-
-% sintagma_preposicional(Oracion, Resultado):- preposicion(Oracion,
-% Res1), adjetivo(Res1, Res2), verbo(Res2, Resultado).
-sintagma_preposicional(Oracion, Resultado):- preposicion(Oracion, Res1), sintagma_nominal(Res1, Resultado).
+% Predicado para probar oraciones
+test :- write("Ingrese una oración: "), read(Oracion), oracion(Oracion, Resultado), write("La oración es válida."), nl, nl, test.
 
 
-sintagma_descriptivo(Oracion, Resultado):- adverbio(Oracion, Res1), verbo(Res1, Resultado).
-% sintagma_descriptivo(Oracion, Resultado):- verbo(Oracion, Res1),
-% adjetivo(Res1, Resultado).
-sintagma_descriptivo(Oracion, Resultado):- adverbio(Oracion, Res1), determinante(Res1, Resultado, _).
-
-sintagma_nominal(Oracion, Resultado):-determinante(Oracion, Res1, Genero), sustantivo(Res1, Resultado, Genero).
-sintagma_nominal(Oracion, Resultado):-determinante(Oracion, Res1, _), sustantivo(Res1, Resultado, _). %Sin tomar en cuenta el género
-sintagma_nominal(Oracion, Resultado):-determinante(Oracion, Res1, Genero), verbo(Res1, Resultado).
-
-sintagma_verbal(Oracion, Resultado):-verbo(Oracion, Resultado).
-sintagma_verbal(Oracion, Resultado):-verbo(Oracion, Res1), sintagma_nominal(Res1, Resultado).
-sintagma_verbal(Oracion, Resultado):-verbo(Oracion, Res1), sintagma_nominal(Res1, Resultado).
-sintagma_verbal(Oracion, Resultado):-verbo(Oracion, Res1), sintagma_nominal(Res1, Resultado).
-sintagma_verbal(Oracion, Resultado):-verbo(Oracion, Res1), sintagma_nominal(Res1, Resultado).
+%test :-
+   % write('Ingrese una oración: '),
+    %read(X),
+    %oracion(X, []),
+    %write('La oración es válida.'),
+    %nl,
+    %test.
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-% Regla de prueba
-test :- write('Ingrese una oración: '), read(X), oracion(X, []), write('La oración es válida.'), nl, test.
 
 
 
--- baseBNF.pl
+++ baseBNF.pl
@@ -1,7 +1,7 @@
 % Elementos léxicos
-:-consult(basese).
+:-consult('basese.pl').
 
-%Saludos
+% Saludos
 saludar1(["Hola"|Rest], Rest).
 saludar1(["hola"|Rest], Rest).
 saludar1(["Hey"|Rest], Rest).
@@ -9,14 +9,14 @@ saludar1(["hey"|Rest], Rest).
 
 saludar2(["Buenos"|Rest], Rest).
 saludar2(["buenos"|Rest], Rest).
-saludo4(["dias"|Rest], Rest).
+saludar3(["dias"|Rest], Rest).
 
-saludar3(["Buenas"|Rest], Rest).
-saludar3(["buenas"|Rest], Rest).
-saludo5(["tardes"|Rest], Rest).
-saludo5(["noches"|Rest], Rest).
+saludar4(["Buenas"|Rest], Rest).
+saludar4(["buenas"|Rest], Rest).
+saludar5(["tardes"|Rest], Rest).
+saludar5(["noches"|Rest], Rest).
 
-%Despedidas
+% Despedidas
 despedir(["Adios"|Rest], Rest).
 despedir(["adios"|Rest], Rest).
 despedir(["Chao"|Rest], Rest).
@@ -30,27 +30,18 @@ despedir(["Gracias"|Rest], Rest).
 despedir(["Hasta"|Rest], Rest).
 despedir(["hasta"|Rest], Rest).
 
-%Prerposiciones
+% Preposiciones
 preposicion(["En"|Rest], Rest).
 preposicion(["en"|Rest], Rest).
 
-
-%Verbos (arreglarlos)
-
-%%Verbo
+% Verbos
 verbo(["comer"|Rest], Rest).
 verbo(["desayunar"|Rest], Rest).
 verbo(["almorzar"|Rest], Rest).
 verbo(["cenar"|Rest], Rest).
 verbo(["tomar"|Rest], Rest).
-
-
 verbo(["visitar"|Rest], Rest).
 verbo(["querer"|Rest], Rest).
-%conjugado(querer, queremos, n). %n -> nosotros
-
-
-
 verbo(["Quiero"|Rest], Rest).
 verbo(["quiero"|Rest], Rest).
 verbo(["Quisiera"|Rest], Rest).
@@ -66,18 +57,7 @@ verbo(["estamos"|Rest], Rest).
 
 sinonimo(tomar, beber).
 
-
-
-
-preposicion(["Me"|Rest], Rest).
-preposicion(["me"|Rest], Rest).
-preposicion(["Nos"|Rest], Rest).
-preposicion(["nos"|Rest], Rest).
-preposicion(["Les"|Rest], Rest).
-preposicion(["les"|Rest], Rest).
-preposicion(con).
-
-%Pronombres
+% Pronombres
 pronombre(["Yo"|Rest], Rest).
 pronombre(["yo"|Rest], Rest).
 pronombre(["Ella"|Rest], Rest).
@@ -91,12 +71,8 @@ pronombre(["ellos"|Rest], Rest).
 pronombre(["Ellas"|Rest], Rest).
 pronombre(["ellas"|Rest], Rest).
 
-
-
-
-
-%Adverbios
-aderbio(['hoy'|Rest], Rest).
+% Adverbios
+adverbio(['hoy'|Rest], Rest).
 adverbio(['mañana'|Rest], Rest).
 adverbio(['cerca'|Rest], Rest).
 adverbio(['lejos'|Rest], Rest).
@@ -112,17 +88,14 @@ adverbio(['No'|Rest], Rest).
 adverbio(['si'|Rest], Rest).
 adverbio(['Si'|Rest], Rest).
 
-
-
-
+% Sustantivos
 sustantivo(['mascotas'|Rest], Rest, f).
 sustantivo(['opcion'|Rest], Rest, f).
 sustantivo(['opciones'|Rest], Rest, f).
-
 sustantivo(['comida'|Rest], Rest, f).
 sustantivo(['comida rápida'|Rest], Rest, f).
-sustantivo(['maricos'|Rest], Rest, m).
-sustantivo([ Rest|'italiana'], Rest, f).
+sustantivo(['mariscos'|Rest], Rest, m).
+sustantivo([Rest|'italiana'], Rest, f).
 sustantivo([Rest|'japonesa'], Rest, f).
 sustantivo([Rest|'argentina'], Rest, f).
 sustantivo([Rest|'alemana'], Rest, f).
@@ -131,10 +104,7 @@ sustantivo([Rest|'mexicana'], Rest, f).
 sustantivo([Rest|'vegana'], Rest, f).
 sustantivo([Rest|'Comida gourmet y opciones sin gluten'], Rest, f).
 
-
-
-sugerencias(podrías).
-
+% Determinantes
 determinante(["alguna"|Rest], Rest, f).
 determinante(["alguno"|Rest], Rest, m).
 determinante(["la"|Rest], Rest, f).
@@ -149,20 +119,16 @@ determinante(["tengo"|Rest], Rest, no).
 
 
 
-% adverbios(acaso, quiza, probablemente, posiblemente, tampoco, nunca,
-% no, jamas, cerca, aqui, lejos, ahi, entre, bastante, mucho, demasiado,
-% mas, menos, poco, nada, muy, apenas, luego, hoy, ayer, tarde, temprano,
-% antier, pronto, antes, si, cierto, tambien, claro, regular, bien, mal,
-% mejor, peor, asi, siempre).
-% conjunciones(y, e, ni, pero, aunque, sino, o, u, si, como, que,
-% mientras, luego, pues, porque).
-% preposiciones(a, ante, bajo, con, contra, de, desde, durante, en,
-% entre, hacia, hasta, para, por, segun, durante, mediante, sobre, sin,
-% tras, pro, con, tras).
-%interjecciones(si, no, oh, ay, uf, hola, adios).
-% sustantivos(gato, perro, casa, mesa, silla, libro, auto, ciudad, pais,
-% persona).
-% adjetivos(grande, pequeño, hermoso, feo, feliz, triste, inteligente,
-% tonto, divertido, aburrido).
+
+
+
+
+
+
+
+
+
+
+
 
 
